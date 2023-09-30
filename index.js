3993d5df4728fa6f6d5ab1583250a1d4d157040d
Md Ariful Islam <111215572+arifulrifat12@users.noreply.github.com>
2023-09-30 20:54:21 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+
+
+// var result = prompt("Enter your Marks");
+// if (result>=80)
+//     document.write("A+");
+// else if(result>=70)
+//     document.write("A");
+// else if(result>=60)
+//     document.write("A-");
+// else if(result>=50)
+//     document.write("B");
+// else if(result>=40)
+//     document.write("C");
+// else if(result>=33)
+//     document.write("D");
+// else
+//     document.write("Fail");
+
+
+
+// var letter = prompt("Enter Your Letter :");
+// letter = letter.toLowerCase();
+
+// if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+//     document.write("vowel");
+// else
+//     document.write("consonant");
+
+var digit = prompt("Enter Your digit");
+
+switch(digit){
+    case "0":
+      document.write("zero");
+      break;
+    case "1":
+      document.write("one");
+      break;
+    case "2":
+      document.write("Two");
+      break;
+    case "3":
+      document.write("Three");
+      break;
+    case "4":
+      document.write("Four");
+      break;
+    case "5":
+      document.write("Five");
+      break;
+    case "6":
+      document.write("Six");
+      break;
+    case "7":
+      document.write("Seven");
+      break;
+    case "8":
+      document.write("Eight");
+      break;
+    case "9":
+      document.write("Nine");
+      break;
+    default :
+        document.write("Invalid");
+}
